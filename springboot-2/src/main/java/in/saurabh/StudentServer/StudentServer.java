a6fa6a3c114ea77e854a789eeada931e4f7a8b24
saurabh kumar <saurabhkaswa04@gmail.com>
2026-07-14 13:02:39 +0530

    StudentServer: Post route with Student

--- springboot-2/src/main/java/in/saurabh/StudentServer/StudentServer.java
+++ springboot-2/src/main/java/in/saurabh/StudentServer/StudentServer.java
@@ -2,19 +2,23 @@ package in.saurabh.StudentServer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentServer {
     // 1. Store the student
     @PostMapping("/create")
-    public String storeStudent(){
-        return """
-                id: 1
-                name: Saurabh
-                Departement: CSE
-                age: 20
-                """;
+    public String storeStudent(@RequestBody Student student){
+        int id = student.getId();
+        String name = student.getName();
+        String department = student.getDepartment();
+        int age = student.getAge();
+
+        return "Id: "+id+", Name: "+name+", age: "+age+", department: "+department;
+
+
+
     }
 
     // 2. Read the Student with id
